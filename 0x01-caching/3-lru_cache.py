85fd8c90c7ba7d65b67b40c2af64b8480775b2b0
arkoaikins <arkoaikins@gmail.com>
2024-01-31 21:45:38 -0500

    Create a class LRUCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Create a class LRUCache that inherits from
+BaseCaching and is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Inherits from BaseCaching"""
+    def __init__(self):
+        super().__init__()
+        self.access_list = []
+
+    def put(self, key, item):
+        """
+        assigns to the dictionary self.cache_data the
+        item value for the key key.
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discarded_key = self.access_list.pop(0)
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key}")
+
+        self.cache_data[key] = item
+        self.access_list.append(key)
+
+    def get(self, key):
+        """
+        returns the value in self.cache_data linked to key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.access_list.remove(key)
+        self.access_list.append(key)
+
+        return self.cache_data[key]
