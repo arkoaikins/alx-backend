f0cf6cce261266ababa60dd44ae105916965ef6c
arkoaikins <arkoaikins@gmail.com>
2024-01-31 21:23:44 -0500

    cache system that returns value cache linked to key

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Create a class BasicCache that inherits from
+BaseCaching and is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Inherits from BaseCaching"""
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        assigns to the dictionary self.cache_data the
+        item value for the key key
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        returns the value in self.cache_data linked to key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
