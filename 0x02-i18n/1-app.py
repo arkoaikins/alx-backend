d04802b60ac3689a161da794e8de35869afc2081
arkoaikins <arkoaikins@gmail.com>
2024-02-06 12:27:05 -0500

    use babel to configure available lang and timezones

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+A basic flask app with a single route that renders to
+index.html template
+And use babel to help with default locale(lang) and timezone
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Class config to set Babels default local
+    and timezone
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route("/")
+def index():
+    """display home page"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
